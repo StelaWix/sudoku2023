738113e3e9e1ba4268339146d7a432c99389dd76
StelaWix <132207389+StelaWix@users.noreply.github.com>
2023-04-30 22:24:53 +0600

    Добавьте файлы проекта.

---
+++ sudoku/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SudokuCell = new System.Windows.Forms.Panel();
+            this.AdvancedLevel = new System.Windows.Forms.RadioButton();
+            this.IntermediateLevel = new System.Windows.Forms.RadioButton();
+            this.beginnerLevel = new System.Windows.Forms.RadioButton();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SudokuCell
+            // 
+            this.SudokuCell.Location = new System.Drawing.Point(13, 13);
+            this.SudokuCell.Name = "SudokuCell";
+            this.SudokuCell.Size = new System.Drawing.Size(380, 380);
+            this.SudokuCell.TabIndex = 0;
+            // 
+            // AdvancedLevel
+            // 
+            this.AdvancedLevel.AutoSize = true;
+            this.AdvancedLevel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AdvancedLevel.Location = new System.Drawing.Point(423, 208);
+            this.AdvancedLevel.Name = "AdvancedLevel";
+            this.AdvancedLevel.Size = new System.Drawing.Size(106, 25);
+            this.AdvancedLevel.TabIndex = 1;
+            this.AdvancedLevel.TabStop = true;
+            this.AdvancedLevel.Text = "Сложный";
+            this.AdvancedLevel.UseVisualStyleBackColor = true;
+            // 
+            // IntermediateLevel
+            // 
+            this.IntermediateLevel.AutoSize = true;
+            this.IntermediateLevel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.IntermediateLevel.Location = new System.Drawing.Point(423, 239);
+            this.IntermediateLevel.Name = "IntermediateLevel";
+            this.IntermediateLevel.Size = new System.Drawing.Size(99, 25);
+            this.IntermediateLevel.TabIndex = 2;
+            this.IntermediateLevel.TabStop = true;
+            this.IntermediateLevel.Text = "Средний";
+            this.IntermediateLevel.UseVisualStyleBackColor = true;
+            // 
+            // beginnerLevel
+            // 
+            this.beginnerLevel.AutoSize = true;
+            this.beginnerLevel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.beginnerLevel.Location = new System.Drawing.Point(423, 270);
+            this.beginnerLevel.Name = "beginnerLevel";
+            this.beginnerLevel.Size = new System.Drawing.Size(86, 25);
+            this.beginnerLevel.TabIndex = 3;
+            this.beginnerLevel.TabStop = true;
+            this.beginnerLevel.Text = "Легкий";
+            this.beginnerLevel.UseVisualStyleBackColor = true;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newGameButton.Location = new System.Drawing.Point(422, 311);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(160, 55);
+            this.newGameButton.TabIndex = 4;
+            this.newGameButton.Text = "Новая Игра";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click_1);
+            // 
+            // checkButton
+            // 
+            this.checkButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkButton.Location = new System.Drawing.Point(423, 63);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(160, 55);
+            this.checkButton.TabIndex = 5;
+            this.checkButton.Text = "Проверка";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click_1);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(423, 136);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(160, 55);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(447, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "00:00:00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(422, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 55);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Решение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(178, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 411);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Лучший результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.beginnerLevel);
+            this.Controls.Add(this.IntermediateLevel);
+            this.Controls.Add(this.AdvancedLevel);
+            this.Controls.Add(this.SudokuCell);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SudokuCell;
+        private System.Windows.Forms.RadioButton AdvancedLevel;
+        private System.Windows.Forms.RadioButton IntermediateLevel;
+        private System.Windows.Forms.RadioButton beginnerLevel;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ sudoku/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Common;
+using System.Collections;
+
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        public static string connectString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =бд.mdb";
+        /// </summary>
+        private OleDbConnection myConnection;
+        System.Timers.Timer timer;
+        int h;
+        int m;
+        int s;
+        int TimeRaiting;
+        string LevelGame;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            createCells();
+            startNewGame();
+            
+            
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+
+        }
+
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimeEvent;
+        }
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                TimeRaiting = s + (m * 60) + (h * 60);
+                textBox1.Text = string.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            }));
+        }
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                { 
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Black;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    SudokuCell.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        private void startNewGame()
+        {
+            loadValues();
+            s = 0;
+            m = 0;
+            h = 0;
+            var hintsCount = 0;
+
+            if (beginnerLevel.Checked)
+                hintsCount = 45;
+            else if (IntermediateLevel.Checked)
+                hintsCount = 30;
+            else if (AdvancedLevel.Checked)
+                hintsCount = 15;
+
+            showRandomValuesHints(hintsCount);
+        }
+
+        private void showRandomValuesHints(int hintsCount)
+        {
+            // Показывать значение в случайных ячейках
+            // Количество подсказок зависит от выбранного игроком уровня
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                // Оформите ячейки с подсказками по-другому и
+                // заблокируйте ячейку, чтобы игрок не мог отредактировать значение
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+
+        private void loadValues()
+        {
+           
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+
+          
+            findValueForNextCell(0, -1);
+        }
+
+        Random random = new Random();
+
+        private bool findValueForNextCell(int i, int j)
+        {
+           
+            if (++j > 8)
+            {
+                j = 0;
+
+                if (++i > 8)
+                    return true;
+            }
+
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        
+            do
+            {
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    
+                    return false;
+                }
+
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                numsLeft.Remove(value);
+            }
+            while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+
+            return true;
+        }
+
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void checkButton_Click_1(object sender, EventArgs e)
+        {
+            var wrongCells = new List<SudokuCell>();
+
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+
+            
+
+            if (wrongCells.Any())
+            {
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Wrong inputs / неверно");
+            }
+            else
+            {
+                MessageBox.Show("You Wins / Победа");
+                if (beginnerLevel.Checked)
+                {
+                    LevelGame = "Beginner";
+                }
+                if (IntermediateLevel.Checked)
+                {
+                    LevelGame = "Intermediate";
+                }
+                if (AdvancedLevel.Checked)
+                {
+                    LevelGame = "Advanced";
+                }
+                string query = "INSERT INTO'" + LevelGame + "'(s) VALUES('" + TimeRaiting + "')";
+
+            }
+            timer.Stop();
+           
+           
+
+        }
+
+
+
+ 
+
+        private void newGameButton_Click_1(object sender, EventArgs e)
+        {
+            startNewGame();
+            timer.Start();
+            if (beginnerLevel.Checked)
+            {
+                OleDbCommand command = new OleDbCommand("SELECT MIN(s) FROM [Beginner]", myConnection);
+                command.ExecuteNonQuery();
+                
+                OleDbDataReader reader = command.ExecuteReader();
+                
+                label1.Text = "";
+
+                while (reader.Read())
+                {
+                    label1.Text = reader[0].ToString()+" сек";
+                }
+
+          
+                reader.Close();
+
+
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+            Application.DoEvents(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            decisionGame();
+           
+        }
+
+        private void clearButton_Click_1(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void decisionGame()
+        {
+            // Показывать значение в случайных ячейках
+            // Количество подсказок зависит от выбранного игроком уровня
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var rX = i;
+                    var rY = j;
+
+
+
+                    // Оформите ячейки с подсказками по-другому и
+                    // заблокируйте ячейку, чтобы игрок не мог отредактировать значение
+                    cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                    cells[rX, rY].ForeColor = Color.Black;
+                    cells[rX, rY].IsLocked = true;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
